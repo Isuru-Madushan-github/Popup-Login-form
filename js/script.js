0fc95dafe68d5d0eb992148041fd88b32d9da616
Isuru-Madushan-github <misuru543@gmail.com>
2022-11-22 17:47:00 +0530

    Popup Login Form

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openForm(){
+    document.querySelector('.login-form').style.display="block";
+}
+
+function closeForm(){
+    document.querySelector('.login-form').style.display="none";
+}
